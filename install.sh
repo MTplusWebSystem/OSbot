ab1039511959349f94fabc9d0bb51971d1012e11
MTplusWebSystem <128556678+MTplusWebSystem@users.noreply.github.com>
2023-07-10 04:44:08 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
-curl -s https://deb.nodesource.com/setup_18.x
-sudo apt install nodejs -y
-npm update
-apt install npm -y
-npm i rompot
+sudo apt install -y curl;
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt-get install -y nodejs
+npm install -g npm@9.8.0
 apt install screen -y
+git clone https://github.com/MTplusWebSystem/OSbot
+cd OSbot
 screen -S OSbot -dm node index.js
